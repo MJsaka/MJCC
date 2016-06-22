c9a73747bdce39753e8400fe5efa789dba3c6505
MJsaka <qiuxuenan@gmail.com>
2016-06-23 00:56:20 +0800

    简单测试界面设计

---
+++ MJCC/TreeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TreeViewController.swift
+//  MJCC
+//
+//  Created by MJsaka on 16/6/22.
+//  Copyright © 2016年 MJsaka. All rights reserved.
+//
+
+import UIKit
+
+class TreeViewController: UIViewController , UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    var tree : EquationTree!
+    var treeView : TreeView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var size = TreeView.minimumSize(tree.root as! EquationNode)
+        if size.height < self.view.frame.height {
+            size.height = self.view.frame.height
+        }
+
+        treeView = TreeView(frame: CGRect(origin: CGPoint(x: 0 , y: 0), size: size))
+        treeView.backgroundColor = UIColor.whiteColor()
+        treeView.root = tree.root;
+        self.scrollView.contentSize = size
+        self.scrollView.delegate = self
+        self.scrollView.addSubview(treeView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return treeView
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MJCC/ViewController.swift
+++ MJCC/ViewController.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    var input : String!
+    var lexer : Lexer!
+    var parser : Parser!
+    var tree : EquationTree!
+    
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func calculate(sender: UIBarButtonItem) {
+        input = inputTextView.text
+        lexer  = Lexer(input:input)
+        parser  = Parser(input: lexer)
+        tree  = parser.parse()
+        inputTextView.text = tree.equationString()
+        tree.transformForVariable(0)
+        outputTextView.text = tree.equationString()
+        resultLabel.text = tree.variables[0].token.text + " = \(tree.subEquationValue(node: tree.root.rightChild as! EquationNode))"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowTreeView" {
+            if self.tree == nil {
+                input = inputTextView.text
+                lexer  = Lexer(input:input)
+                parser  = Parser(input: lexer)
+                tree  = parser.parse()
+            }
+            let treeViewController = segue.destinationViewController as! TreeViewController
+            treeViewController.tree = self.tree
+        }
     }
 
 
